6489d09af9160b8c7e404522239dafdf7f049883
Davis Kemlers <davis.kemlers@icloud.com>
2020-04-13 21:31:13 +0900

    Create React Appを利用してプロジェクトの初期化
    - Reduxに必要なパケージの追加
    - リンティング用に"standard"パケージの追加
    - jsconfigを追加して絶対パスを利用可能にする
    - このプロジェクトに必要ないCreate React Appが初期に生成するファイルの削除

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './style.css'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <div>初期テンプレート</div>
+  </React.StrictMode>,
+  document.getElementById('root')
+)
